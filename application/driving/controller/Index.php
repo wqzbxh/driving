210ab02564ffce9da5a1bb886f301310c62aa365
wanghaiyang <wqzbxh@163.com>
2018-07-05 23:48:10 +0800

    生成二维码

--- application/driving/controller/Index.php
+++ application/driving/controller/Index.php
@@ -1,10 +1,29 @@
 <?php
-namespace app\index\controller;
+namespace app\driving\controller;
 
 class Index
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
+        //vendor引入类
+       vendor('phpqrcode.phpqrcode');
+        //生成二维码
+        
+       
+       //实例化二维码类
+       $object = new \QRcode();
+       $url = '普秋真，你热不热啊，我这有空调！';
+       $level=3;
+       $size=4;
+        $pathname = 'public' . DS . 'code'  .date('Y-m-d') .DS ;
+        if(!is_dir($pathname)) { //若目录不存在则创建之
+        mkdir($pathname);
+        }
+        
+        $ad = $pathname  . rand(10000,99999) . ".png";
+        $errorCorrectionLevel =intval($level) ;//容错级别
+        $matrixPointSize = intval($size);//生成图片大小
+       return  $object->png($url, $ad, $errorCorrectionLevel, $matrixPointSize, 2);
+        
     }
 }
